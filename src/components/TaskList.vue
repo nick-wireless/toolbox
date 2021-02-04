c4614a8bd9bd57bb0e304fea9ed5b0dd36920f69
Nick Hanigan <australian.nick@gmail.com>
2021-02-04 20:47:11 +0930

    using vuex with storybook

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="list-items">
-    <template v-if="loading">
-      loading
-    </template>
-    <template v-else-if="isEmpty">
-      empty
-    </template>
-    <template v-else>
-      <Task v-for="task in tasks" :key="task.id" :task="task" v-on="$listeners" />
-    </template>
-  </div>
+  <PureTaskList :tasks="tasks" v-on="$listeners" @archive-task="archiveTask" @pin-task="pinTask" />
 </template>
 
 <script>
-  import Task from './Task';
+  import PureTaskList from './PureTaskList.vue';
+  import { mapState, mapActions } from 'vuex';
+
   export default {
-    name: 'TaskList',
-    components: { Task },
-    props: {
-      tasks: { type: Array, required: true, default: () => [] },
-      loading: { type: Boolean, default: false },
-    },
-    computed: {
-      isEmpty() {
-        return this.tasks.length === 0;
-      },
-    },
+    components: { PureTaskList },
+
+    methods: mapActions(['archiveTask', 'pinTask']),
+
+    computed: mapState(['tasks']),
   };
 </script>
